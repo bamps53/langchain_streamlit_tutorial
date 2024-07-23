f549969ea3a87ac63e11c128698668ac7a3c8463
bamps53 <daisuke0530@gmail.com>
2024-07-23 14:59:53 +0000

    remove langchain

---
+++ llm.py
@@ -0,0 +1,121 @@
+from openai import OpenAI
+from google.generativeai import GenerativeModel
+from groq import Groq
+from anthropic import Anthropic
+from pydantic import BaseModel
+from loguru import logger
+
+OPENAI_MODELS = ["gpt-4o-mini", "gpt-4o"]
+GOOGLE_MODELS = ["gemini-1.5-pro-latest", "gemini-1.5-flash"]
+GROQ_MODELS = ["gemma2-9b-it", "llama3-groq-70b-8192-tool-use-preview"]
+ANTHROPIC_MODELS = ["claude-3-5-sonnet-20240620"]
+MODELS = OPENAI_MODELS + GOOGLE_MODELS + GROQ_MODELS + ANTHROPIC_MODELS
+
+
+def get_google_messages(messages):
+    google_messages = []
+
+    for message in messages:
+        if message["role"] == "user":
+            google_messages.append({"role": "user", "parts": [message["content"][0]["text"]]})
+        elif message["role"] == "assistant":
+            # TODO 画像を処理する
+            google_messages.append({"role": "model", "parts": [message["content"][0]["text"]]})
+        else:
+            raise NotImplementedError()
+
+    return google_messages
+
+
+class BaseMessage(BaseModel):
+    roll: str = ""
+    content: str
+
+    def as_raw_message(self):
+        return {"role": self.roll, "content": [{"type": "text", "text": self.content}]}
+
+
+class HumanMessage(BaseMessage):
+    roll: str = "user"
+    content: str
+
+
+class SystemMessage(BaseMessage):
+    roll: str = "system"
+    content: str
+
+
+class AIMessage(BaseMessage):
+    roll: str = "assistant"
+    content: str
+
+
+class LLM:
+    def __init__(self, model_name: str, temperature: float = 0.0, max_tokens: int = 8192, stream: bool = True):
+        self.model_name = model_name
+        self.temperature = temperature
+        self.max_tokens = max_tokens
+        self.stream = stream
+
+    def chat(self, messages):
+        messages = [m.as_raw_message() for m in messages]
+        logger.debug(messages)
+
+        if self.model_name in OPENAI_MODELS:
+            client = OpenAI()
+            return client.chat.completions.create(
+                model=self.model_name,
+                messages=messages,
+                temperature=self.temperature,
+                max_tokens=self.max_tokens,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0,
+                stream=self.stream,
+            )
+
+        if self.model_name in GROQ_MODELS:
+            client = Groq()
+            return client.chat.completions.create(
+                model=self.model_name,
+                messages=messages,
+                temperature=self.temperature,
+                max_tokens=self.max_tokens,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0,
+                stream=self.stream,
+            )
+
+        system_message = messages[0]
+        if system_message["role"] != "system":
+            raise ValueError("The first message must be a system message.")
+
+        rest_messages = messages[1:]
+
+        if self.model_name in GOOGLE_MODELS:
+            client = GenerativeModel(
+                self.model_name,
+                generation_config={
+                    "temperature": self.temperature,
+                    "top_p": 0.95,
+                    "top_k": 64,
+                    "max_output_tokens": self.max_tokens,
+                    "response_mime_type": "text/plain",
+                },
+                system_instruction=system_message["content"][0]["text"],
+            )
+            return client.generate_content(contents=get_google_messages(rest_messages), stream=self.stream)
+
+        if self.model_name in ANTHROPIC_MODELS:
+            client = Anthropic()
+            return client.messages.create(
+                model=self.model_name,
+                max_tokens=self.max_tokens,
+                temperature=self.temperature,
+                system=system_message["content"][0]["text"],
+                messages=rest_messages,
+                stream=self.stream,
+            )
+        else:
+            raise ValueError(f"Invalid model name: {self.model_name}")
--- main.py
+++ main.py
@@ -1,23 +1,14 @@
 import streamlit as st
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_groq import ChatGroq
-from langchain_anthropic import ChatAnthropic
-from langchain.schema import SystemMessage, HumanMessage, AIMessage
+from llm import LLM, HumanMessage, SystemMessage, AIMessage, MODELS
 
 load_dotenv()
 
-SYSTEM_MESSAGE = "あなたは極めて陽気な関西人の芸人です。何を質問されても面白おかしく回答してください。"
-OPENAI_MODELS = ["gpt-4o-mini", "gpt-4o"]
-GOOGLE_MODELS = ["gemini-1.5-pro-latest", "gemini-1.5-flash"]
-GROQ_MODELS = ["gemma2-9b-it", "llama3-groq-70b-8192-tool-use-preview"]
-ANTHROPIC_MODELS = ["claude-3-5-sonnet-20240620"]
-MODELS = OPENAI_MODELS + GOOGLE_MODELS + GROQ_MODELS + ANTHROPIC_MODELS
+DEFAULT_SYSTEM_MESSAGE = "あなたはコーディングに特化したAIアシスタントです。"
 
 
-def init_history():
-    st.session_state.messages = [SystemMessage(content=SYSTEM_MESSAGE)]
+def init_history(system_message):
+    st.session_state.messages = [SystemMessage(content=system_message)]
 
 
 def show_history():
@@ -33,45 +24,33 @@ def show_history():
                 st.write(message.content)
 
 
-def build_llm(model_name, temperature):
-    if model_name in OPENAI_MODELS:
-        return ChatOpenAI(model=model_name, temperature=temperature, streaming=True)
-    elif model_name in GOOGLE_MODELS:
-        return ChatGoogleGenerativeAI(model=model_name, temperature=temperature, streaming=True)
-    elif model_name in GROQ_MODELS:
-        return ChatGroq(model=model_name, temperature=temperature, streaming=True)
-    elif model_name in ANTHROPIC_MODELS:
-        return ChatAnthropic(model=model_name, temperature=temperature, streaming=True)
-    else:
-        raise ValueError(f"Invalid model name: {model_name}")
-
-
 def main():
     st.set_page_config(
-        page_title="My ChatGPT",
+        page_title="Code Writer",
         page_icon="🤖",
     )
 
     with st.sidebar:
         model_name = st.radio("Select a model", MODELS)
         temperature = st.slider("Temperature", 0.0, 1.0, 0.0)
+        system_message = st.text_area("System message", DEFAULT_SYSTEM_MESSAGE, height=150)
         clear_history = st.button("Clear chat history")
         if clear_history or "messages" not in st.session_state:
-            init_history()
+            init_history(system_message)
 
     if "messages" not in st.session_state:
-        init_history()
+        init_history(system_message)
 
     show_history()
 
-    llm = build_llm(model_name, temperature)
+    llm = LLM(model_name, temperature)
 
     if user_input := st.chat_input("Type a message..."):
         with st.chat_message("user"):
             st.write(user_input)
         st.session_state.messages.append(HumanMessage(content=user_input))
 
-        stream = llm.stream(st.session_state.messages)
+        stream = llm.chat(st.session_state.messages)
         with st.chat_message("assistant"):
             content = st.write_stream(stream)
         st.session_state.messages.append(AIMessage(content=content))
